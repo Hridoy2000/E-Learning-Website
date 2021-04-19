105d46f58e4c74e73822de932ad63d74b2986f95
Mazharul Islam Hridoy <50412526+Hridoy2000@users.noreply.github.com>
2021-04-20 02:44:08 +0600

    Toggle Button For Changing Background Color

---
+++ color.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Color</title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <link rel="stylesheet" href="color.css">
+</head>
+<body>
+<div>
+	<input type="checkbox" class="checkbox" id="chk" />
+	<label class="label" for="chk">
+		<i class="fas fa-moon"></i>
+		<i class="fas fa-sun"></i>
+		<div class="ball"></div>
+	</label>
+</div>
+<script src="custom.js"></script>
+</body>
+</html>
