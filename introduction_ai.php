b8f5563807b3f403101570d535cbdf70b2705fe6
Mazharul Islam Hridoy <50412526+Hridoy2000@users.noreply.github.com>
2021-04-22 23:51:37 +0600

    Social Icon and Footer.
    You guys should care about the path of file.

---
+++ introduction_ai.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Artificial Intellegence</title>
+    <!--Change The Path-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+    <link href="home copy.css" rel="stylesheet">
+    <link href="deep_Learning.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container  mt-5 justify-content-center">
+  <ul>
+    <li class="facebook">
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <div class="slider">
+        <p>facebook</p>
+      </div>
+    </li>
+        <li class="twitter">
+      <i class="fa fa-twitter" aria-hidden="true"></i>
+      <div class="slider">
+        <p>twitter</p>
+      </div>
+    </li>
+    
+    <li class="instagram">
+      <i class="fa fa-instagram" aria-hidden="true"></i>
+      <div class="slider">
+        <p>instagram</p>
+      </div>
+    </li>
+        <li class="google">
+      <i class="fa fa-google" aria-hidden="true"></i>
+      <div class="slider">
+        <p>google</p>
+      </div>
+    </li>
+        <li class="whatsapp">
+      <i class="fa fa-whatsapp" aria-hidden="true"></i>
+      <div class="slider">
+        <p>whatsapp</p>
+      </div>
+    </li>
+  </ul>
+
+  <footer>
+         <div class="row my-5 justify-content-center py-5">
+             <div class="col-11">
+                 <div class="row ">
+                     <div class="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
+                         <h3 class="text-muted mb-md-0 mb-5 bold-text">Team FootPrint.</h3>
+                         <p class="text-muted mb-md-0 mb-5 bold-text"> Director,Rakib Hasan</p>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
+                         <h6 class="mt-55 mt-2 text-muted bold-text"><b>AKM Sifath Ullah</b></h6><small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> sifath818@gmail.com</small>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3 ">
+                         <h6 class="text-muted bold-text"><b>Taskin Mostafa Azam</b></h6><small><span><i class="fa fa-envelope" aria-hidden="true"></i></span> taskin15@gmail.com</small>
+                     </div>
+                 </div>
+                 <div class="row ">
+                     <div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
+                         <p class="social text-muted mb-0 pb-0 bold-text"> <span class="mx-2"><i class="fa fa-facebook" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></span> </p><small class="rights"><span>&#174;</span> Team FootPrint All Rights Reserved.</small>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
+                         <h6 class="mt-55 mt-2 text-muted bold-text"><b>Mazharul Islam Hridoy</b></h6><small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> hridoy818@gmail.com</small>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3 ">
+                         <h6 class="text-muted bold-text"><b>MD. Sazzad Hossain</b></h6><small><span><i class="fa fa-envelope" aria-hidden="true"></i></span> sazzad15@gmail.com</small>
+                     </div> 
+                 </div>
+             </div>
+         </div>
+     </footer>
+</div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </body>
+
+</html>
