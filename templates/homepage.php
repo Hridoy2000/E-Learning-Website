5501893f7e3ff2d93256458ccd765b2a9cb02add
Mohammod-Sifath <64678172+Mohammod-Sifath@users.noreply.github.com>
2021-04-23 20:03:32 +0600

    We got a chatbot now!!!!!!!!!!!!!!!!!!!!!!

--- templates/homepage.php
+++ templates/homepage.php
@@ -15,7 +15,17 @@
                  <h2 class="Info">Learn Without Limits <br> Build skills with courses, certificates, and degrees online</h2>
             </div>
         </div>
-
+<!-- Start of ChatBot (www.chatbot.com) code -->
+<script type="text/javascript">
+    window.__be = window.__be || {};
+    window.__be.id = "60815308dc6d180007033837";
+    (function() {
+        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+    })();
+</script>
+<!-- End of ChatBot code -->
     {% include 'footer.html' %}          
             
         
